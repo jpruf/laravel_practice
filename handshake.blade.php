49b5752039595a86c13ef39fe8a8a9b8f89b94e5
Jennifer Prufer <jprufer@gmail.com>
2016-08-29 20:05:27 -0700

    deleted files

--- handshake.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <a href="{{route('home')}}">Back</a>
-    <h1>handshake</h1>
-@endsection
--- nice.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <a href="{{route('home')}}">Back</a>
-    <h1>I {{$action }} {{$name}}!!!</h1>
-@endsection
